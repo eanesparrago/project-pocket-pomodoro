1ae25b9bbc73168c750353e8e9aaf72cc418e99c
LJEsp <esparragoleandroj@gmail.com>
2018-10-15 19:59:33 +0800

    Add docs for Github Pages

---
+++ docs/precache-manifest.40787642b2f93f4cad52cc3f9884b905.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1a7ce436502660bef208",
+    "url": "/static/js/main.1a7ce436.chunk.js"
+  },
+  {
+    "revision": "2c9dd40a8e3922546d6b",
+    "url": "/static/js/1.2c9dd40a.chunk.js"
+  },
+  {
+    "revision": "1a7ce436502660bef208",
+    "url": "/static/css/main.a4ba0281.chunk.css"
+  },
+  {
+    "revision": "6383e30fe3057fd40872393c7e5afb21",
+    "url": "/index.html"
+  }
+];
