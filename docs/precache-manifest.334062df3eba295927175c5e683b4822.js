40464516a2caa9eaae01007098fac40a616f261e
LJEsp <esparragoleandroj@gmail.com>
2018-10-15 20:06:21 +0800

    Add docs

---
+++ docs/precache-manifest.334062df3eba295927175c5e683b4822.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7bc264362142bb16e950",
+    "url": "/project-pocket-pomodoro/static/js/runtime~main.7bc26436.js"
+  },
+  {
+    "revision": "08b04e29438652bba980",
+    "url": "/project-pocket-pomodoro/static/js/main.08b04e29.chunk.js"
+  },
+  {
+    "revision": "8c6fc07c3084f8c9008f",
+    "url": "/project-pocket-pomodoro/static/js/1.8c6fc07c.chunk.js"
+  },
+  {
+    "revision": "08b04e29438652bba980",
+    "url": "/project-pocket-pomodoro/static/css/main.a4ba0281.chunk.css"
+  },
+  {
+    "revision": "58dff9319afbc78ad9e4668a3fefe3f2",
+    "url": "/project-pocket-pomodoro/index.html"
+  }
+];
