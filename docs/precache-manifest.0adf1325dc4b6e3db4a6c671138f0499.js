4c833c80aa9b476729f384b4076c0bd828a0ee17
LJEsp <esparragoleandroj@gmail.com>
2018-10-17 16:12:37 +0800

    Update docs

---
+++ docs/precache-manifest.0adf1325dc4b6e3db4a6c671138f0499.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "938be6ed42c09de39698add6a9181f92",
+    "url": "/project-pocket-pomodoro/static/media/notification.938be6ed.mp3"
+  },
+  {
+    "revision": "7bc264362142bb16e950",
+    "url": "/project-pocket-pomodoro/static/js/runtime~main.7bc26436.js"
+  },
+  {
+    "revision": "b53e9718932165efd825",
+    "url": "/project-pocket-pomodoro/static/js/main.b53e9718.chunk.js"
+  },
+  {
+    "revision": "c0b5f2dbdf2e40fafb8c",
+    "url": "/project-pocket-pomodoro/static/js/1.c0b5f2db.chunk.js"
+  },
+  {
+    "revision": "b53e9718932165efd825",
+    "url": "/project-pocket-pomodoro/static/css/main.2b97bb3f.chunk.css"
+  },
+  {
+    "revision": "b5f8e81311f33a15270c6e2938671b8f",
+    "url": "/project-pocket-pomodoro/index.html"
+  }
+];
